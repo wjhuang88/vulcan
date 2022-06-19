e4768bf45d93bcf9471708f2c358aaa38e604278
Huang.Wj <wjhuang@live.cn>
2022-06-19 17:53:29 +0800

    提交http服务器构建框架代码

--- vulcan-net/src/main/java/io/vulcan/net/impl/HttpServer.java
+++ vulcan-net/src/main/java/io/vulcan/net/impl/HttpServer.java
@@ -1,10 +1,26 @@
 package io.vulcan.net.impl;
 
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.HttpServerExpectContinueHandler;
+import io.vulcan.net.CloseHandler;
 import io.vulcan.worker.WorkerPool;
 
 public class HttpServer extends SocketServer {
 
+    public HttpServer(int port) {
+        this(port, WorkerPool.getDefault());
+    }
+
     public HttpServer(int port, WorkerPool pool) {
         super(port, pool);
+    }
+
+    public CloseHandler start() {
+        return super.start(new HttpServerCodec(), new HttpServerExpectContinueHandler(), new HttpServerHandler());
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        HttpServer server = new HttpServer(8888);
+        CloseHandler start = server.start();
     }
 }
